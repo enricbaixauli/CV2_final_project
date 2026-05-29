a148ca931800ba8514158a401c30ab2156f22ccb
Enric Baixauli <enricbaixaulicasan@gmail.com>
2026-05-29 13:03:32 +0200

    dataset.py clean

--- dataset.py
+++ dataset.py
@@ -11,53 +11,6 @@ import numpy as np
 import scipy.io
 import random
 
-class LP300W(Dataset):
-    def __init__(self, image_paths, transform=None, apply_occlusion=True, occlusion_mode="random"):
-        """
-        image_paths: List of file paths to images
-        occlusion_mode: 'random', 'clean', 'mask', 'glasses', or 'both'
-        apply_occlusion: If False, ignores occlusion pipeline completely
-        """
-        self.occlusion_mode = occlusion_mode
-        self.transform = transform
-        self.apply_occlusion = apply_occlusion
-        
-        self.image_paths = []
-        self.mat_paths = []
-
-        for img_path in image_paths:
-            mat_path = img_path.replace(".jpg", ".mat")
-            if os.path.exists(mat_path):
-                self.image_paths.append(img_path)
-                self.mat_paths.append(mat_path)
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
-        mat_path = self.mat_paths[idx]
-
-        img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        pose_mat = sio.loadmat(mat_path)
-        pose = torch.tensor(pose_mat["Pose_Para"][0][:3], dtype=torch.float32) # Yaw, Pitch, Roll
-
-        if self.apply_occlusion:
-            landmarks = utils.extract_landmarks(img_path)
-            img = utils.apply_occlusion(img, landmarks, mode=self.occlusion_mode)
-
-        if self.transform:
-            if not isinstance(img, Image.Image):
-                img = Image.fromarray(img)
-            img = self.transform(img)
-
-        return img, pose
-    
-fa_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-fa = face_alignment.FaceAlignment(face_alignment.LandmarksType.TWO_D, flip_input=False, device=fa_device)
-
 class I2HeadDataset(Dataset):
     def __init__(self, image_paths_or_dir, transform=None, occlusion_mode="raw"):
         """
@@ -84,12 +37,12 @@ class I2HeadDataset(Dataset):
     def __getitem__(self, idx):
         raw_img_path = self.image_paths[idx]
 
-        # Determine which occlusion branch to load
+        # We have 4 branches to coose from
         mode = self.occlusion_mode
         if mode == "random":
             mode = random.choice(["raw", "mask", "glasses", "both"])
 
-        # Construct the target path based on the selected mode
+        # Based on them we determine the path
         if mode == "raw":
             img_path = raw_img_path
         else:
@@ -105,7 +58,6 @@ class I2HeadDataset(Dataset):
                 
             img_path = os.path.join(parent_dir, mode, new_basename + ext)
 
-        # Load the target image variant
         img = cv2.imread(img_path)
         if img is None:
             raise FileNotFoundError(f"Failed to load image: {img_path}")
